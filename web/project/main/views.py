0f3088443b87186ce6870cda408d5cf88e4fa3f1
nishkumar <nk595@cornell.edu>
2017-04-01 02:19:30 -0400

    Add template for index page

--- web/project/main/views.py
+++ web/project/main/views.py
@@ -1,10 +1,14 @@
-from django.shortcuts import render
 from django.http import HttpResponse
-
+from django.shortcuts import render
+from django.template import loader
 
 # endpoint for /index
 def index(request):
-    return HttpResponse("Hello, world. You're at the index page.")
+    #get the template 
+    template = loader.get_template('index.html')
+
+    #render the template in HttpResponse
+    return HttpResponse(template.render())	
 
 
 # endpoint for /login
