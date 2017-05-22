732614913a9a9350b223393352b2f9e6fa9fd6f8
nishkumar <nk595@cornell.edu>
2017-05-22 19:16:31 -0400

    Set default view index.

--- web/project/main/urls.py
+++ web/project/main/urls.py
@@ -4,7 +4,4 @@ from . import views
 
 urlpatterns = [
     url(r'^index', views.index, name='index'),
-    url(r'^login', views.login, name='login'),
-    url(r'^logout', views.logout, name='logout'),
-    url(r'^update', views.update, name='update'),
 ]
--- web/project/main/views.py
+++ web/project/main/views.py
@@ -9,18 +9,3 @@ def index(request):
 
     #render the template in HttpResponse
     return HttpResponse(template.render())	
-
-
-# endpoint for /login
-def login(request):
-    return HttpResponse("Hello, world. This is the login endpoint.")
-
-
-# endpoint for /logout
-def logout(request):
-    return HttpResponse("Hello, world. This is the logout endpoint.")
-
-
-# endpoint for /update
-def update(request):
-    return HttpResponse("Hello, world. This is the update api.")
