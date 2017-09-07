0cdac10ee51cc3e812ae9188606301e6be0644ee
nishkumar <nk595@cornell.edu>
2017-09-06 20:38:13 -0400

    Fix default url bug

--- web/project/main/urls.py
+++ web/project/main/urls.py
@@ -4,7 +4,6 @@ from rest_framework_jwt import views as jwt_views
 from . import views
 
 urlpatterns = [
-    url(r'', views.index, name='index'),
     url(r'^home/', views.index, name='index'),
     # Authentication APIs
     url(r'^api/auth', jwt_views.obtain_jwt_token, name="auth"),
@@ -25,4 +24,6 @@ urlpatterns = [
     url(r'^api/edit_answer/(?P<record>\d+)/(?P<question>\d+)$', views.AnswerUpdateView.as_view(), name="edit_answer"),
     url(r'^api/edit_record/(?P<pk>\d+)$', views.RecordUpdateView.as_view(), name="edit_record"),
     url(r'^api/edit_question/(?P<pk>\d+)$', views.QuestionUpdateView.as_view(), name="edit_question"),
+    # Default URL
+    url(r'', views.index, name='index'),
 ]
