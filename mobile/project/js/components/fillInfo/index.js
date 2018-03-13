ff3e727e3e8e16e3e455f8ff4628083a4cf888d5
Laisa <laisa.barros26@gmail.com>
2018-03-13 17:19:33 -0400

    finish adaptive design on Registration and Login; fix buttons on home page

--- mobile/project/js/components/fillInfo/index.js
+++ mobile/project/js/components/fillInfo/index.js
@@ -1,10 +1,19 @@
-
 import React, { Component } from 'react';
+import { Actions } from 'react-native-router-flux';
 import { Image } from 'react-native';
 import { connect } from 'react-redux';
-import { Container, Content, Item, Button, Icon, View, Text, Radio, InputGroup} from 'native-base';
-import { Actions } from 'react-native-router-flux';
-import { Grid, Row,Col } from 'react-native-easy-grid';
+import { Container,
+         Content,
+         Item,
+         Button,
+         Icon,
+         View,
+         Text,
+         InputGroup,
+         Label } from 'native-base';
+import { Grid,
+         Row,
+         Col } from 'react-native-easy-grid';
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
 import { setUser, registerUser } from '../../actions/RegisterUser';
 import styles from './styles';
@@ -33,6 +42,7 @@ class FillInfo extends Component {
         selectedOption,
         caregiver,
         street,
+        sector,
         city,
         myState,
         country
@@ -48,6 +58,7 @@ class FillInfo extends Component {
           doctor,
           caregiver,
           street,
+          sector,
           city,
           myState,
           country,
@@ -65,76 +76,86 @@ class FillInfo extends Component {
       <Container>
         <KeyboardAwareScrollView style={styles.container}>
           <Content>
-            <Text style={styles.title}>
-              Account Information
-            </Text>
-              
-                <Item regular style={styles.list}>
+                <Text style={styles.title}>
+                Account Information
+                </Text>
+                <Item stackedLabel regular style={styles.list}>
+                    <Label>First Name*</Label>
                     <TextField
                         style={styles.input}
-                        placeholder='First Name *'
+                        placeholder='e.g. John'
                         value={this.props.firstName}
                         onChangeText={value => this.props.setUser({
                             prop: 'firstName', value
                         })}
                     />
                 </Item>
-
-                <Item regular style={styles.list}>
+                <Item stackedLabel regular style={styles.list}>
+                    <Label>Last Name*</Label>
                     <TextField
                         style={styles.input}
-                        placeholder='Last Name *'
+                        placeholder='e.g. Smith'
                         value={this.props.lastName}
                         onChangeText={value => this.props.setUser({
                             prop: 'lastName', value
                         })}
                     />
                 </Item>
-
-                <Item regular style={styles.list}>
+                <Item stackedLabel regular style={styles.list}>
+                    <Label>Street*</Label>
                     <TextField
                         style={styles.input}
-                        placeholder='Street *'
+                        placeholder='123 Street St'
                         value={this.props.street}
                         onChangeText={value => this.props.setUser({
                             prop: 'street', value
                         })}
                     />
                 </Item>
-
-                <Item regular style={styles.list}>
+                <Item stackedLabel regular style={styles.list}>
+                    <Label>Sector*</Label>
                     <TextField
                         style={styles.input}
-                        placeholder='City *'
+                        placeholder='Gisenyi'
+                        value={this.props.sector}
+                        onChangeText={value => this.props.setUser({
+                            prop: 'sector', value
+                        })}
+                    />
+                </Item>
+                <Item stackedLabel regular style={styles.list}>
+                    <Label>City*</Label>
+                    <TextField
+                        style={styles.input}
+                        placeholder='City'
                         value={this.props.city}
                         onChangeText={value => this.props.setUser({
                             prop: 'city', value
                         })}
                     />
                 </Item>
-
-                <Item regular style={styles.list}>
+                <Item stackedLabel regular style={styles.list}>
+                    <Label>State*</Label>
                     <TextField
                         style={styles.input}
-                        placeholder='State *'
+                        placeholder='State'
                         value={this.props.myState}
                         onChangeText={value => this.props.setUser({
                             prop: 'myState', value
                         })}
                     />
                 </Item>
-
-                <Item regular style={styles.list}>
+                <Item stackedLabel regular style={styles.list}>
+                    <Label>Country*</Label>
                     <TextField
                         style={styles.input}
-                        placeholder='Country *'
+                        placeholder='Country'
                         value={this.props.country}
                         onChangeText={value => this.props.setUser({
                             prop: 'country', value
                         })}
                     />
                 </Item>
-
                 <Item regular style={styles.list}>
                     <SegmentedControls
                         tint={'#F16C00'}
@@ -145,76 +166,73 @@ class FillInfo extends Component {
                         selectedOption={ this.props.selectedOption }
                     />
                 </Item>
-
-                  <Item regular style={styles.list}>
-                    <TextField
-                        style={styles.input}
-                        placeholder='Email *'
-                        value={this.props.email}
-                        onChangeText={value => this.props.setUser({
-                            prop: 'email', value
-                        })}
-                    />
-                  </Item>
-
-                  <Item regular style={styles.list}>
+                <Item stackedLabel regular style={styles.list}>
+                      <Label>Email*</Label>
+                      <TextField
+                          style={styles.input}
+                          placeholder='email@address.com'
+                          value={this.props.email}
+                          onChangeText={value => this.props.setUser({
+                              prop: 'email', value
+                          })}
+                      />
+                </Item>
+                <Item stackedLabel regular style={styles.list}>
+                    <Label>PIN*</Label>
                     <TextField
                         secureTextEntry
                         style={styles.input}
-                        placeholder='Password *'
+                        placeholder='******'
                         value={this.props.password}
                         onChangeText={value => this.props.setUser({
                             prop: 'password', value
                         })}
                     />
-                  </Item>
-
-                  <Item regular style={styles.list}>
+                </Item>
+                <Item stackedLabel regular style={styles.list}>
+                    <Label>Mobile*</Label>
                     <TextField
                         style={styles.input}
-                        placeholder='Mobile *'
+                        placeholder='555-555-5555'
                         value={this.props.mobile}
                         onChangeText={value => this.props.setUser({
                             prop: 'mobile', value
                         })}
                     />
-                  </Item>
-                
-                 <Item regular style={styles.list}>
+                </Item>
+                <Item stackedLabel regular style={styles.list}>
+                    <Label>My diagnosis*</Label>
                     <TextField
                         style={styles.input}
-                        placeholder='My diagnosis *'
+                        placeholder='Diagnosis'
                         value={this.props.diagnosis}
                         onChangeText={value => this.props.setUser({
                             prop: 'diagnosis', value
                         })}
                     />
-                  </Item>
-
-                 <Item regular style={styles.list}>
+                </Item>
+                <Item stackedLabel regular style={styles.list}>
+                    <Label>My Care Giver*</Label>
                     <TextField
                         style={styles.input}
-                        placeholder='My Care Giver *'
+                        placeholder='My Care Giver'
                         value={this.props.caregiver}
                         onChangeText={value => this.props.setUser({
                             prop: 'caregiver', value
                         })}
                     />
-                  </Item>
-                 
-                  <Item regular style={styles.list}>
+                </Item>
+                <Item stackedLabel regular style={styles.list}>
+                    <Label>My Doctor's Name*</Label>
                     <TextField
                         style={styles.input}
-                        placeholder='My doctor name *'
+                        placeholder='John Smith'
                         value={this.props.doctor}
                         onChangeText={value => this.props.setUser({
                             prop: 'doctor', value
                         })}
                     />
-                  </Item>
-                
-             
-
+                </Item>
                 <Grid>
                     <Col>
                         <Button rounded bordered style={styles.bottom} onPress={() => Actions.login()}>
@@ -246,6 +264,7 @@ const mapStateToProps = (state) => {
         selectedOption,
         caregiver,
         street,
+        sector,
         city,
         myState,
         country
@@ -262,6 +281,7 @@ const mapStateToProps = (state) => {
         selectedOption,
         caregiver,
         street,
+        sector,
         city,
         myState,
         country
