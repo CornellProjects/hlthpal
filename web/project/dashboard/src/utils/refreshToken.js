6a26c4b10c07720efa94c6b8a112bcf699315421
seye <shayibanky@gmail.com>
2018-04-08 15:17:34 -0400

    update for 10 mins

---
+++ web/project/dashboard/src/utils/refreshToken.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+import setAuthorizationToken from '../utils/setAuthorizationToken';
+
+export default function refreshToken(){
+  function fetchToken(){
+    axios.post('api/token-refresh',{'token':localStorage.jwtToken}).then(
+          res => {
+            const token = res.data.token;
+            localStorage.setItem('jwtToken', token);
+            setAuthorizationToken(token);
+          }
+        )
+  }
+  setInterval(fetchToken, 10 * 60 * 1000) // in milliseconds
+}
