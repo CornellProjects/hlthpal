06607dd679426f0c0773d940c494253b6d394a60
seye <shayibanky@gmail.com>
2018-04-08 15:16:05 -0400

    add view for single patient detail

---
+++ web/project/dashboard/src/routes.js
@@ -0,0 +1,9 @@
+const routes = {
+  '/': 'Home',
+  '/patients':'Patients',
+   '/viewPatient':'View Patient',
+  '/addPatients':'Add Patients',
+  '/addDoctors':'Add Doctors',
+  '/addHospitals':'Add Hospitals'
+};
+export default routes;
