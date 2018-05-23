7f0d3a52fe9e82366802042e921ecb091612020b
seye <shayibanky@gmail.com>
2018-05-23 16:14:52 -0400

    add full credentials to include username, email and first name

--- mobile/project/js/cred.js
+++ mobile/project/js/cred.js
@@ -1,3 +1,4 @@
-export const moi_username = 'test@email.com';
+export const moi_username = 'test';
+export const moi_email = 'test@gmail.com';
 export const moi_password = 'test1234';
 export const moi_firstname = 'Beatrice';
