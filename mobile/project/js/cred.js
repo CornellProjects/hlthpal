4042fa00f731f54e880141721c4238059b003770
seye <shayibanky@gmail.com>
2018-05-23 19:20:06 -0400

    add more than one user offline detail

--- mobile/project/js/cred.js
+++ mobile/project/js/cred.js
@@ -1,4 +1,44 @@
-export const moi_username = 'test';
-export const moi_email = 'test@gmail.com';
-export const moi_password = 'test1234';
-export const moi_firstname = 'Beatrice';
+//export const {user, user0, user1, user2, user3, user4}
+
+user = {
+            'username':'test',
+            'email': 'test@gmail.com',
+            'password': 'test1234',
+            'first_name': 'Cassandra'
+        };
+user0 = {
+            'username':'test0',
+            'email': 'test0@gmail.com',
+            'password': 'test1234',
+            'first_name': 'Margaret'
+        };
+user1 = {
+            'username':'test1',
+            'email': 'test1@gmail.com',
+            'password': 'test1234',
+            'first_name': 'Fatima'
+        };
+user2 = {
+            'username':'test2',
+            'email': 'test2@gmail.com',
+            'password': 'test1234',
+            'first_name': 'Lovette'
+        };
+user3 = {
+            'username':'test3',
+            'email': 'test3@gmail.com',
+            'password': 'test1234',
+            'first_name': 'Durette'
+        };
+user4 = {
+            'username':'test4',
+            'email': 'test4@gmail.com',
+            'password': 'test1234',
+            'first_name': 'Casipas'
+        }
+
+function users(){
+    return [user, user1, user2, user3, user4];
+}
+
+export {users};
