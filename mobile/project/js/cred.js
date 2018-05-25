9763a9b2871750304f3fba1acb84dce4bbf19257
seye <shayibanky@gmail.com>
2018-05-25 01:52:50 -0400

    email and username same

--- mobile/project/js/cred.js
+++ mobile/project/js/cred.js
@@ -7,31 +7,31 @@ user = {
             'first_name': 'Cassandra'
         };
 user0 = {
-            'username':'test0',
+            'username':'test0@gmail.com',
             'email': 'test0@gmail.com',
             'password': 'test1234',
             'first_name': 'Margaret'
         };
 user1 = {
-            'username':'test1',
+            'username':'test1@gmail.com',
             'email': 'test1@gmail.com',
             'password': 'test1234',
             'first_name': 'Fatima'
         };
 user2 = {
-            'username':'test2',
+            'username':'test2@gmail.com',
             'email': 'test2@gmail.com',
             'password': 'test1234',
             'first_name': 'Lovette'
         };
 user3 = {
-            'username':'test3',
+            'username':'test3@gmail.com',
             'email': 'test3@gmail.com',
             'password': 'test1234',
             'first_name': 'Durette'
         };
 user4 = {
-            'username':'test4',
+            'username':'test4@gmail.com',
             'email': 'test4@gmail.com',
             'password': 'test1234',
             'first_name': 'Casipas'
