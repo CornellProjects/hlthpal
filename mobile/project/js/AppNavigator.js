6dc1849240278a10efbaa1c4c22d5312ed1bef6c
seye <shayibanky@gmail.com>
2018-10-10 08:52:29 -0400

    add routes for language pages

--- mobile/project/js/AppNavigator.js
+++ mobile/project/js/AppNavigator.js
@@ -6,26 +6,43 @@ import { Router, Scene } from 'react-native-router-flux';
 import { closeDrawer } from './actions/drawer';
 import Login from './components/login/';
 import Home from './components/home/';
+import Home_K from './components/home_kinya/';
 import BlankPage from './components/blankPage';
 import SideBar from './components/sideBar';
 import { statusBarColor } from './themes/base-theme';
 import QtwoOne from './components/qtwoOne';
+import QtwoOne_K from './components/qtwoOne_kinya';
 import QtwoTwo from './components/qtwoTwo';
+import QtwoTwo_K from './components/qtwoTwo_kinya';
 import QtwoThree from './components/qtwoThree';
+import QtwoThree_K from './components/qtwoThree_kinya';
 import QtwoFour from './components/qtwoFour';
+import QtwoFour_K from './components/qtwoFour_kinya';
 import QtwoFive from './components/qtwoFive';
+import QtwoFive_K from './components/qtwoFive_kinya';
 import QtwoSix from './components/qtwoSix';
+import QtwoSix_K from './components/qtwoSix_kinya';
 import QtwoSeven from './components/qtwoSeven';
+import QtwoSeven_K from './components/qtwoSeven_kinya';
 import QtwoEight from './components/qtwoEight';
+import QtwoEight_K from './components/qtwoEight_kinya';
 import QtwoNine from './components/qtwoNine';
+import QtwoNine_K from './components/qtwoNine_kinya';
 import QtwoTen from './components/qtwoTen';
+import QtwoTen_K from './components/qtwoTen_kinya';
 import QtwoEleven from './components/qtwoEleven';
+import QtwoEleven_K from './components/qtwoEleven_kinya';
 import QtwoTwelve from './components/qtwoTwelve';
+import QtwoTwelve_K from './components/qtwoTwelve_kinya';
 import otherSymptoms from './components/otherSymptoms';
+import otherSymptoms_K from './components/otherSymptoms_kinya';
 import FillInfo from './components/fillInfo';
 import RecordList from './components/RecordList';
 import symptomsForm from './components/symptomsForm';
+import symptomsForm_K from './components/symptomsForm_kinya';
 import Instructions from './components/instructions';
+import Instructions_K from './components/instructions_kinya';
+import Languages from './components/languages';
 
 const RouterWithRedux = connect()(Router);
 
@@ -62,42 +79,76 @@ class AppNavigator extends Component {
         return <Login />;
       case 'home':
         return <Home />;
+      case 'home_k':
+        return <Home_K />;
       case 'blankPage':
         return <BlankPage />;
       case 'fillInfo':
         return <FillInfo />;
       case 'qtwoOne':
         return <QtwoOne />
+      case 'qtwoOne_k':
+        return <QtwoOne_K />
       case 'qtwoTwo':
         return <QtwoTwo />
+      case 'qtwoTwo_k':
+        return <QtwoTwo_K />
       case 'qtwoThree':
         return <QtwoThree />
+      case 'qtwoThree_k':
+        return <QtwoThree_K />
       case 'qtwoFour':
         return <QtwoFour />
+      case 'qtwoFour_k':
+        return <QtwoFour_K />
       case 'qtwoFive':
         return <QtwoFive />
+      case 'qtwoFive_k':
+        return <QtwoFive_K />
       case 'qtwoSix':
         return <QtwoSix />
+      case 'qtwoSix_k':
+        return <QtwoSix_K />
       case 'qtwoSeven':
         return <QtwoSeven />
+      case 'qtwoSeven_k':
+        return <QtwoSeven_K />
       case 'qtwoEight':
         return <QtwoEight />
+      case 'qtwoEight_k':
+        return <QtwoEight_K />
       case 'qtwoNine':
         return <QtwoNine />
+      case 'qtwoNine_k':
+        return <QtwoNine_K />
       case 'qtwoTen':
         return <QtwoTen />
+      case 'qtwoTen_k':
+        return <QtwoTen_K />
       case 'qtwoEleven':
         return <QtwoEleven />
+      case 'qtwoEleven_k':
+        return <QtwoEleven_K />
       case 'qtwoTwelve':
         return <QtwoTwelve />
+      case 'qtwoTwelve_k':
+        return <QtwoTwelve_K />
       case 'otherSymptoms':
         return <otherSymptoms />
+      case 'otherSymptoms_k':
+        return <otherSymptoms_K />
       case 'symptomsForm':
         return <symptomsForm />
+      case 'symptomsForm_k':
+        return <symptomsForm_K />
       case 'RecordList':
         return <RecordList />
       case 'Instructions':
         return <Instructions />
+      case 'Instructions_K':
+        return <Instructions_K />
+      case 'Languages':
+        return <Languages />
       default:
         return <Login />;
     }
@@ -141,23 +192,40 @@ class AppNavigator extends Component {
             <Scene key="login" component={Login} hideNavBar initial />
             <Scene key="fillInfo" component={FillInfo} />
             <Scene key="home" component={Home} />
+            <Scene key="home_k" component={Home_K} />
             <Scene key="blankPage" component={BlankPage} />
             <Scene key="qtwoOne" component={QtwoOne} />
+            <Scene key="qtwoOne_k" component={QtwoOne_K} />
             <Scene key="qtwoTwo" component={QtwoTwo} />
+            <Scene key="qtwoTwo_k" component={QtwoTwo_K} />
             <Scene key="qtwoThree" component={QtwoThree} />
+            <Scene key="qtwoThree_k" component={QtwoThree_K} />
             <Scene key="qtwoFour" component={QtwoFour} />
+            <Scene key="qtwoFour_k" component={QtwoFour_K} />
             <Scene key="qtwoFive" component={QtwoFive} />
+            <Scene key="qtwoFive_k" component={QtwoFive_K} />
             <Scene key="qtwoSix" component={QtwoSix} />
+            <Scene key="qtwoSix_k" component={QtwoSix_K} />
             <Scene key="qtwoSeven" component={QtwoSeven} />
+            <Scene key="qtwoSeven_k" component={QtwoSeven_K} />
             <Scene key="qtwoEight" component={QtwoEight} />
+            <Scene key="qtwoEight_k" component={QtwoEight_K} />
             <Scene key="qtwoNine" component={QtwoNine} />
+            <Scene key="qtwoNine_k" component={QtwoNine_K} />
             <Scene key="qtwoTen" component={QtwoTen} />
+            <Scene key="qtwoTen_k" component={QtwoTen_K} />
             <Scene key="qtwoEleven" component={QtwoEleven} />
+            <Scene key="qtwoEleven_k" component={QtwoEleven_K} />
             <Scene key="qtwoTwelve" component={QtwoTwelve} />
+            <Scene key="qtwoTwelve_k" component={QtwoTwelve_K} />
             <Scene key="otherSymptoms" component={otherSymptoms} />
+            <Scene key="otherSymptoms_k" component={otherSymptoms_K} />
             <Scene key="symptomsForm" component={symptomsForm} />
+            <Scene key="symptomsForm_k" component={symptomsForm_K} />
             <Scene key="RecordList" component={RecordList} />
             <Scene key="Instructions" component={Instructions} />
+            <Scene key="Instructions_K" component={Instructions_K} />
+            <Scene key="Languages" component={Languages} />
           </Scene>
         </RouterWithRedux>
       </Drawer>
